22c48d58b71e21daa5a070208523d23b991ec9a0
Naomi Leow <naomileowwx@gmail.com>
2014-06-20 01:15:40 +0800

    interaction between applauncher and updatemanager done

--- src/htlauncher/launcher/AppLauncher.java
+++ src/htlauncher/launcher/AppLauncher.java
@@ -1,5 +1,8 @@
 package htlauncher.launcher;
 
+import java.io.IOException;
+import java.net.URI;
+
 import htlauncher.updater.UpdateManager;
 
 public class AppLauncher {
@@ -9,24 +12,33 @@ public class AppLauncher {
 	private Process appProcess;
 	
 	public static void main(String[] args) {
-		
-		
+		AppLauncher launcher = new AppLauncher();
+		//uncomment when ready
+//		launcher.run();
 	}
 	
 	public AppLauncher(){
-		
+		updater = new UpdateManager(AppLauncher.APP_INFO_FILEPATH);
 	}
 	
 	public void run(){
-		
+		runUpdater();
+		launchApp();
 	}
 	
 	public void runUpdater(){
-		
+		updater.runUpdate();
 	}
 	
 	public void launchApp(){
-		
+		URI launchPath = updater.getAppLaunchPath();
+		String command = "java -jar " + launchPath.toString(); 
+		try {
+			appProcess = Runtime.getRuntime().exec(command);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/htlauncher/updater/UpdateManager.java
+++ src/htlauncher/updater/UpdateManager.java
@@ -1,35 +1,53 @@
 package htlauncher.updater;
 
-import htlauncher.utilities.AppDescriptor;
 
-import java.io.File;
+import htlauncher.utilities.ComponentDescriptor;
+
 import java.net.URI;
-import java.util.HashMap;
+import java.util.Set;
 
 public class UpdateManager {
 	
-	private AppDescriptor appDescriptor;
 	private UpdateDataManager dataManager;
 	
 	public UpdateManager(String appInfoPath){
 		dataManager = new UpdateDataManager(appInfoPath);
 	}
 	
-	public URI getAppLaunchPath(){
-		return appDescriptor.getLaunchPath();
-	}
-	
 	public void runUpdate(){
-		
+		updateAppDetails();
+		updateAppComponents();
 	}
 	
 	public void updateAppDetails(){
-		
+		URI serverURI = dataManager.serverAppInfoURI;
+		if(serverURI != null){
+			//overwrite with file from server
+		}
+		dataManager.loadAppData();
 	}
 	
 	public void updateAppComponents(){
-		
+		for(ComponentDescriptor component: dataManager.getAppComponents()){
+			updateComponent(component);
+		}
+		dataManager.saveUpdaterData();
 	}
 	
+	public void updateComponent(ComponentDescriptor component){
+		String name = component.getComponentName();
+		double latestVer = component.getVersion();
+		double currentVer = dataManager.getDownloadedVersion(name);
+		if(latestVer > currentVer){
+			//update jar for component from server
+			
+			//update success: update downloaded component version
+			dataManager.updateDownloadedVersion(name, latestVer);
+		}
+	}
+	
+	public URI getAppLaunchPath(){
+		return dataManager.getAppLaunchPath();
+	}
 	
 }
